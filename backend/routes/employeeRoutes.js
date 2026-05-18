2cb3ae55563470ddd71e3e32c32ba2fa0ebdbdcd
Yash-Bansal-10 <yash.2428cseai2270@kiet.edu>
2026-05-18 10:24:57 +0530

    feat: add JWT auth middleware and protect employee routes

--- backend/routes/employeeRoutes.js
+++ backend/routes/employeeRoutes.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const { addEmployee, getEmployees, searchEmployees } = require('../controllers/employeeController');
+const { protect } = require('../middleware/authMiddleware');
 
 // Search route must be above /:id if we had one, but good practice
-router.get('/search', searchEmployees);
+router.get('/search', protect, searchEmployees);
 
 router.route('/')
-    .get(getEmployees)
-    .post(addEmployee);
+    .get(protect, getEmployees)
+    .post(protect, addEmployee);
 
 module.exports = router;
